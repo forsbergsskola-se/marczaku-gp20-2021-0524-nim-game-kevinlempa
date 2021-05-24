c2448cde29a13dd52c1c2956781c8d86bb0f388f
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-05-25 00:03:04 +0200

    prep

---
+++ nim.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Nim {
+    public:
+    int numberOfMatches;
+     void Start() {
+        numberOfMatches = 24;
+        MultiplayerMatch();
+    }
+    void MultiplayerMatch() {
+       }
+};
+int main()
+{
+   Nim nim;
+   nim.Start();
+  
+  
+}
