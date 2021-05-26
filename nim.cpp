97b2b01823b2c156e3859cd3da8611e8c8dd83cb
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-05-27 00:19:06 +0200

    improves encapsulation

--- nim.cpp
+++ nim.cpp
@@ -5,47 +5,9 @@
 using namespace std;
 
 
-class Nim
-{
-public:
-	void Start()
-	{
-		char choice;
-		cout << "Would you like to play vs AI : y/n ?" << endl;
-		cin >> choice;
-		switch (choice)
-		{
-		case 'y' :
-			PlayWithAI(true);
-			break;
-		case 'n' : 
-			PlayWithAI(false);
-			break;
-		default:
-			cin.clear();
-			cin.ignore(256, '\n');
-			Start();
-		}
-	}
-	void PlayWithAI(bool ai)
-	{
-
-		int numberOfMatches = 24;
-		bool winner = false;
-		int n;
-		bool playerTurn = true;
-		do {
-			srand(time(NULL));
-			string player = playerTurn ? "Player 1" : "Player 2";
-			if(ai)
-				player = playerTurn ? "Player 1" : "AI";
-			playerTurn = !playerTurn;
-			string matches = "";
-			PlayNim(numberOfMatches, ai, matches, player, n, winner, playerTurn);
-		} while (!winner);
-	}
-	void PlayNim(int& numberOfMatches,bool& ai, std::string& matches, std::string& player, int& n, bool& winner, bool playerTurn)
-	{
+class Nim{
+private:
+	void PlayNim(int& numberOfMatches, bool& ai, std::string& matches, std::string& player, int& n, bool& winner, bool playerTurn) {
 		if (numberOfMatches > 0) {
 			matches = "";
 			for (int i = 0; i < numberOfMatches; i++) {
@@ -65,11 +27,11 @@ public:
 
 			if (n >= 1 && n <= 3) {
 				if (n > numberOfMatches) {
-					cout << player+" removed " << numberOfMatches << " stick(s)" << endl;
+					cout << player + " removed " << numberOfMatches << " stick(s)" << endl;
 					numberOfMatches = 0;
 				}
 				else
-					cout << player+" removed " << n << " stick(s)" << endl;
+					cout << player + " removed " << n << " stick(s)" << endl;
 				numberOfMatches -= n;
 			}
 			else {
@@ -87,10 +49,47 @@ public:
 			}
 		}
 	}
+public:
+	void StartGame()
+	{
+		char choice;
+		cout << "Would you like to play vs AI : y/n ?" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 'y' :
+			PlayWithAI(true);
+			break;
+		case 'n' : 
+			PlayWithAI(false);
+			break;
+		default:
+			cin.clear();
+			cin.ignore(256, '\n');
+			StartGame();
+		}
+	}
+	void PlayWithAI(bool ai){
+
+		int numberOfMatches = 24;
+		bool winner = false;
+		int n;
+		bool playerTurn = true;
+		do {
+			srand(time(NULL));
+			string player = playerTurn ? "Player 1" : "Player 2";
+			if(ai)
+				player = playerTurn ? "Player 1" : "AI";
+			playerTurn = !playerTurn;
+			string matches = "";
+			PlayNim(numberOfMatches, ai, matches, player, n, winner, playerTurn);
+		} while (!winner);
+	}
+
 };
 
 int main()
 {
 	Nim nim;
-	nim.Start();
+	nim.StartGame();
 }
