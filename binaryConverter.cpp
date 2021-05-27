f83ab51162fc777e733583c5f917dd8fe6da0e15
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-05-28 00:07:10 +0200

    adds decimal conversion

--- binaryConverter.cpp
+++ binaryConverter.cpp
@@ -1,9 +1,26 @@
 using namespace std;
+#include <iostream>
 
-class BinaryConverter{
+class BinaryConverter {
 private:
 public:
-}
-int main() {
+	void decToBinary(int n) {
 
+		int binaryNum[32];
+		int i = 0;
+		cout << "0b";
+		while (n > 0) {
+			binaryNum[i] = n % 2;
+			n = n / 2;
+			i++;
+		}
+		for (int j = i - 1; j >= 0; j--)
+			cout << binaryNum[j];
+	}
+
+
+};
+int main() {
+	BinaryConverter bin;
+	bin.decToBinary(25);
 }
