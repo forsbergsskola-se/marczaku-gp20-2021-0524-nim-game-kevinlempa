84d23755f88896ad1f066b19f2caafa183d2a981
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-05-28 00:46:46 +0200

    adds bit operator function and a function to get a number.

--- binaryConverter.cpp
+++ binaryConverter.cpp
@@ -16,11 +16,40 @@ public:
 		}
 		for (int j = i - 1; j >= 0; j--)
 			cout << binaryNum[j];
+		cout << endl;
 	}
-
-
+	int getNumber() {
+		int number = 0;
+		while (true) {
+			cout << "Write a positive number :" << endl;
+			cin >> number;
+			if (cin.fail()) {
+				cin.clear();
+				cin.ignore(256, '\n');
+				cout << "Invalid input try again!" << endl;
+			}
+			else return number;
+		}
+	}
+	void decToBinaryExtra(int n) {
+		int i = 0;
+		bool printZero = false;
+		cout << "0b";
+		for (int i = 31; i >= 0; i--) {
+			int k = n >> i;
+			if (k & 1) {
+				cout << "1";
+				printZero = true;
+			}
+			else if(printZero)
+				cout << "0";
+		}
+	}
 };
 int main() {
 	BinaryConverter bin;
-	bin.decToBinary(25);
+	int n = bin.getNumber();
+	bin.decToBinary(n);
+	bin.decToBinaryExtra(n);
+	return 0;
 }
